dccbd1f5cd22acbb29b5b694711be48666723b23
Vedant Puri <vedantpuri@gmail.com>
2022-09-19 17:10:33 -0400

    done

--- homeworks/hw2.jl
+++ homeworks/hw2.jl
@@ -18,29 +18,33 @@ function dudt_nl(u::AbstractArray; λ=1f0)
     diver(rhs)
 end
 
+# λ
+l1 = 1f-1
+l2 = 1f+1
+
 ln  = euler_fwd(img, dudt_ln; dt=0.01f0, niter=200)
-nl_lam01 = euler_fwd(img, u -> dudt_nl(u; λ=1.0f0 ); dt=0.01f0, niter=200)
-nl_lam10 = euler_fwd(img, u -> dudt_nl(u; λ=10.0f0); dt=0.01f0, niter=200)
+nl1 = euler_fwd(img, u -> dudt_nl(u; λ=l1); dt=0.01f0, niter=200)
+nl2 = euler_fwd(img, u -> dudt_nl(u; λ=l2); dt=0.01f0, niter=200)
 
 p0 = heatmap(img[end:-1:begin, :]; clims=(0,1), c=:grays)
-p1 = heatmap(ln[end:-1:begin, :]; clims=(0,1), c=:grays)
-p2 = heatmap(nl_lam01[end:-1:begin, :]; clims=(0,1), c=:grays)
-p3 = heatmap(nl_lam10[end:-1:begin, :]; clims=(0,1), c=:grays)
+p1 = heatmap(ln[end:-1:begin, :] ; clims=(0,1), c=:grays)
+p2 = heatmap(nl1[end:-1:begin, :]; clims=(0,1), c=:grays)
+p3 = heatmap(nl2[end:-1:begin, :]; clims=(0,1), c=:grays)
 
 p0 = plot!(p0, title="Original")
 p1 = plot!(p1, title="Laplacian Filter, dt=0.01, 200 iters")
-p2 = plot!(p2, title="Nonlinear Filter, dt=0.01, 200 iters, λ=1")
-p3 = plot!(p3, title="Nonlinear Filter, dt=0.01, 200 iters, λ=10")
+p2 = plot!(p2, title="Nonlinear Filter, dt=0.01, 200 iters, λ=$l1")
+p3 = plot!(p3, title="Nonlinear Filter, dt=0.01, 200 iters, λ=$l2")
 ##
 
 filename = "foot_smooth"
 
 save(filename * "_ln" * ".pgm", ln)
-save(filename * "_nl_lam01" * ".pgm", nl_lam01)
-save(filename * "_nl_lam10" * ".pgm", nl_lam10)
+save(filename * "_nl1" * ".pgm", nl1)
+save(filename * "_nl2" * ".pgm", nl2)
 
 savefig(p0, "foot")
 savefig(p1, filename * "_ln")
-savefig(p2, filename * "_nl_lam01")
-savefig(p3, filename * "_nl_lam10")
+savefig(p2, filename * "_nl1")
+savefig(p3, filename * "_nl2")
 #
