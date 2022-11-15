05c8d0085f7077f1494e3f189d6ae5a48c2a818b
Vedant Puri <vedantpuri@gmail.com>
2022-11-14 19:11:30 -0500

    problem reading rawiv

--- project1/vol.jl
+++ project1/vol.jl
@@ -4,5 +4,5 @@ using LinearAlgebra
 filename = "mri_dcm.rawiv_subunit_00.rawiv"
 
 io = open(filename)
-x = reinterpret(Float64, read(io, sizeof(Float64)*10))
+x = reinterpret(Float32, read(io, sizeof(Float32)*32))
 x .= ntoh.(x)
